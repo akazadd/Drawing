15951982b3f7609d8899fd10c6dea0fc7ebed2b6
akazadd <akazad2668@gmail.com>
2023-08-06 13:09:57 +0600

    Shapes, Arc, Circle animation using SwiftUI

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Abul Kalam Azad on 3/8/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
